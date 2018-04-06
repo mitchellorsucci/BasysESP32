17818e9a631c1c7d58ebc16c646daf857f62495e
Mitchell Orsucci <morsucci@uw.edu>
2018-04-06 11:36:33 -0700

    Removed some files

--- simpleServer.py
+++ simpleServer.py
@@ -92,6 +92,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
 
-httpd = BaseHTTPServer.HTTPServer(('192.168.1.104', 4443), MyHandler)
+httpd = BaseHTTPServer.HTTPServer(('192.168.1.107', 4443), MyHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile = './server.pem', server_side = True)
 httpd.serve_forever()
